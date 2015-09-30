33d628103b7c0cd962bf64be92e1c55a6d7ee55e
Administrator <Administrator@scsbwin-575124.ITTPA.EBIZ.VERIZON.COM>
2015-09-30 09:24:11 -0700

    deploy

--- src/com/app/web/tracker/constant/Constant.java
+++ src/com/app/web/tracker/constant/Constant.java
@@ -2,7 +2,7 @@ package com.app.web.tracker.constant;
 
 public class Constant {
 	public static String Login_URI = "/app/login";
-	public static String MAIN_URI = "/Tracker";
+	public static String MAIN_URI = "";
 	public static String HOME_URI = "/app/welcome";
 	public static String CLIENT_URI = "/client/";
 }
